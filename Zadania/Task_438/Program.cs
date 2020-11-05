b4674126a89099e4d39bc2b1f4d2b61251c32ba8
Rafał Kubik <rafass1000@gmail.com>
2020-11-05 15:09:52 +0100

    Init 3

--- Zadania/Task_438/Program.cs
+++ Zadania/Task_438/Program.cs
@@ -6,7 +6,7 @@ namespace Task_438
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!2");
         }
     }
 }
