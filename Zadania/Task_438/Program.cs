3bf6aa0db0a6f376283279d21231a06c2792331d
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-11-05 16:16:53 +0100

    Zadanie 438

--- Zadania/Task_438/Program.cs
+++ Zadania/Task_438/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
-
+﻿
 namespace Task_438
+
 {
-    class Program
+    internal class Program
+
+
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello!");
+            bool x = true;
+
+            while (x)
+
+            {
+                Console.WriteLine("Podaj liczbę od 1 do 100000.");
+
+                string n = Console.ReadLine();
+
+                x = int.TryParse(n, out var result);
+
+                if (!x || result < 1 || result > 100000)
+
+                {
+                    Console.WriteLine("Podano błędną wartość");
+
+                    break;
+                }
+
+                CheckNumber(result);
+            }
+        }
+
+
+        private static void CheckNumber(int number)
+
+        {
+            for (int i = 2; i < number; i++)
+
+                if (number % i == 0)
+
+                {
+                    Console.WriteLine("NIE");
+
+                    return;
+                }
+
+            Console.WriteLine("TAK");
         }
     }
 }
