a66e195740df4ce7b3c9e3a1f7c15875354c3e47
Magdalena Kajszczak <41114872+magdakajszczak@users.noreply.github.com>
2020-11-05 15:34:45 +0100

    próba

--- Zadania/Task_438/Program.cs
+++ Zadania/Task_438/Program.cs
@@ -6,7 +6,7 @@ namespace Task_438
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!2");
+            Console.WriteLine("Hello!");
         }
     }
 }
