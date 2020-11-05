9522be2ecd33a3fc05cfe47a263fd619a5dba178
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-11-05 17:26:40 +0100

    moje zadanie

--- Zadania/Task_499/Program.cs
+++ Zadania/Task_499/Program.cs
@@ -6,7 +6,7 @@ namespace Task_499
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("moje zadanie!");
         }
     }
 }
