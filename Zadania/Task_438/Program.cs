1951e67650016e6bcdfd532ef665aa0ff6f4b1fb
Rafał Kubik <rafass1000@gmail.com>
2020-11-05 16:19:45 +0100

    Code Cleanup

--- Zadania/Task_438/Program.cs
+++ Zadania/Task_438/Program.cs
@@ -1,51 +1,35 @@
-﻿
-namespace Task_438
+﻿using System;
 
+namespace Task_438
 {
     internal class Program
-
-
     {
         private static void Main(string[] args)
-
         {
             bool x = true;
-
             while (x)
-
             {
                 Console.WriteLine("Podaj liczbę od 1 do 100000.");
-
                 string n = Console.ReadLine();
-
                 x = int.TryParse(n, out var result);
-
                 if (!x || result < 1 || result > 100000)
-
                 {
                     Console.WriteLine("Podano błędną wartość");
-
                     break;
                 }
-
                 CheckNumber(result);
             }
         }
 
 
         private static void CheckNumber(int number)
-
         {
             for (int i = 2; i < number; i++)
-
                 if (number % i == 0)
-
                 {
                     Console.WriteLine("NIE");
-
                     return;
                 }
-
             Console.WriteLine("TAK");
         }
     }
