be2ed81c7efbe095ff2a6a60a4ddb407cc962dc9
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-11-19 16:18:23 +0100

    add 549 and 663

---
+++ Zadania/T5ask_549_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Tak_549
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = Convert.ToInt32(Console.ReadLine());
+            int[] tablicaSum = new int[liczbaTestow];
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                int ilosc = Convert.ToInt32(Console.ReadLine());
+                int[] tablicaLiczb = new int[ilosc];
+
+                string[] tablicaZnakow = Console.ReadLine().Split(" ");
+                for (int j = 0; j < ilosc; j++)
+                {
+                    tablicaLiczb[j] = Convert.ToInt32(tablicaZnakow[j]);
+                }
+                tablicaSum[i] = tablicaLiczb.Sum();
+            }
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                Console.WriteLine(tablicaSum[i]);
+            }
+        }
+    }
+}
---
+++ Zadania/Task_663_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_663
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = Convert.ToInt32(Console.ReadLine());
+
+            var listaPunktow = new List<KeyValuePair<string, double>>[liczbaTestow];
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                int liczbaPuntow = Convert.ToInt32(Console.ReadLine());
+                var test = new List<KeyValuePair<string, double>>();
+                for (int j = 0; j < liczbaPuntow; j++)
+                {
+                    var punkt = Console.ReadLine();
+                    var x = Convert.ToInt32(punkt.Split(' ')[1]);
+                    var y = Convert.ToInt32(punkt.Split(' ')[2]);
+
+                    var odleglosc = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                    test.Add(new KeyValuePair<string, double>(punkt, odleglosc));
+                }
+                listaPunktow[i] = test;
+                Console.ReadLine();
+            }
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                listaPunktow[i].Sort((a, b) => a.Value.CompareTo(b.Value));
+                foreach (var pair in listaPunktow[i])
+                {
+                    Console.WriteLine(pair.Key);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
