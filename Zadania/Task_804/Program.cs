af4c5dd8df8a8e4daa22cf689c9a97747d63ecb4
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-11-19 16:56:51 +0100

    task 804 sollution

--- Zadania/Task_804/Program.cs
+++ Zadania/Task_804/Program.cs
@@ -6,7 +6,31 @@ namespace Task_804
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int liczbaPartii = int.Parse(Console.ReadLine());
+
+            var tablicaWynikow = new int[liczbaPartii];
+            for (int i = 0; i < liczbaPartii; i++)
+            {
+                string zetony = Console.ReadLine();
+                var a = int.Parse(zetony.Split(' ')[0]);
+                var b = int.Parse(zetony.Split(' ')[1]);
+                while (a!=b)
+                {
+                    if (a>b)
+                    {
+                        a -= b;
+                    }
+                    else
+                    {
+                        b -= a;
+                    }
+                }
+                tablicaWynikow[i] = a+b;
+            }
+            for (int i = 0; i < liczbaPartii; i++)
+            {
+                Console.WriteLine(tablicaWynikow[i]);
+            }
         }
     }
 }
