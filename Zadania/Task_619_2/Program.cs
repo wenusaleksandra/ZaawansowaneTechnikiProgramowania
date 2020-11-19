becdd9f0316af09eeb6b2e2ac4ca0a9d8e540af7
Aleksandra Wenus <aleksandra.wenus@kotrak.com>
2020-11-19 15:36:57 +0100

    add task 619

---
+++ Zadania/Task_619_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_619
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczbaTestow = int.Parse(Console.ReadLine());
+
+            var tablicaWynikow = new string[liczbaTestow];
+
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                float liczba = float.Parse(Console.ReadLine().Replace('.', ','));
+                string wynik = BitConverter.ToString(BitConverter.GetBytes(liczba));
+                string[] tab = wynik.Split('-');
+                wynik = "";
+                for (int j = tab.Length - 1; j >= 0; j--)
+                {
+                    if (tab[j][0] == '0')
+                    {
+                        wynik += tab[j][1];
+                    }
+                    else
+                    {
+                        wynik += tab[j];
+                    }
+
+                    if (j > 0)
+                    {
+                        wynik += " ";
+                    }
+                }
+                tablicaWynikow[i] = wynik;
+                Console.WriteLine(wynik.ToLower());
+            }
+            for (int i = 0; i < liczbaTestow; i++)
+            {
+                Console.WriteLine(tablicaWynikow[i].ToLower());
+            }
+        }
+    }
+}
